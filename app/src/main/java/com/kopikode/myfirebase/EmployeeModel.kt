69875229140b7583ef1a978f5e07770f85951a30
KOPI KODE <junadhi@sar.ac.id>
2024-05-20 00:30:11 +0700

    Initial commit

---
+++ app/src/main/java/com/kopikode/myfirebase/EmployeeModel.kt
@@ -0,0 +1,8 @@
+package com.kopikode.myfirebase
+
+data class EmployeeModel(
+    var empId: String? = null,
+    var empName: String? = null,
+    var empAge: String? = null,
+    var empSalary: String? = null
+)
---
+++ app/src/main/java/com/kopikode/myfirebase/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kopikode.myfirebase
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnInsertData: Button
+    private lateinit var btnFetchData: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnInsertData = findViewById(R.id.btnInsertData)
+        btnFetchData = findViewById(R.id.btnFetchData)
+
+        btnInsertData.setOnClickListener {
+            val intent = Intent(this, InsertionActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnFetchData.setOnClickListener {
+            val intent = Intent(this, FetchingActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
+
